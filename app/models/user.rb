356c528233de4c7e61453fef2b2e1e64e23723a7
ken-forest <0619green@gmail.com>
2021-11-23 17:10:59 +0900

    userモデルの４箇所に新たなバリデーション追加
    last_nameとfirst_nameに
    全角ひらがな・漢字のバリデーション
    last_name_kanaとfirst_name_kanaに
    全角カタカナのバリデーション
    を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,12 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :nickname, presence: true
-  validates :last_name, presence: true
-  validates :first_name, presence: true
-  validates :last_name_kana, presence: true
-  validates :first_name_kana, presence: true
+  validates :last_name, presence: true, format:{with: /\A[一-龥ぁ-ん]/ }
+  validates :first_name, presence: true, format:{with: /\A[一-龥ぁ-ん]/ }
+  validates :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/ }
+  validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
 
-  validates :password,format:{with: /\A[a-z\d]{6,}\z/ }
+  validates :password, format:{with: /\A[a-z\d]{6,}\z/ }
   
 end
