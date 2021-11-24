0fdb4d8c6fe28d570111583da8b5f14c4c7a3f5b
ken-forest <0619green@gmail.com>
2021-11-24 21:53:35 +0900

    userモデルのバリデーションを変更
    具体的には、last_nameとfirst_nameのバリデーションを
    「/\A[ぁ-んァ-ヶ一-龥々ー]+\z/ 」
    に変更。

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :nickname, presence: true
-  validates :last_name, presence: true, format:{with: /[^\x01-\x7E\uFF61-\uFF9F]/ }
-  validates :first_name, presence: true, format:{with: /[^\x01-\x7E\uFF61-\uFF9F]/ }
+  validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
+  validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
