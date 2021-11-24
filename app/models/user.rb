31eda04a86984b0bca636e6dc81aba2066f41ef9
ken-forest <0619green@gmail.com>
2021-11-24 18:02:20 +0900

    userモデルのlast_nameとfirst_nameのバリデーションを変更
    具体的には、入力する時に、
    全角ひらがな・全角カタカナ・漢字を許可するように変更。
    正規表現を以下のように上書きした。
    
    /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
          
   validates :nickname, presence: true
-  validates :last_name, presence: true, format:{with: /\A[一-龥ぁ-ん]\z/ }
-  validates :first_name, presence: true, format:{with: /\A[一-龥ぁ-ん]\z/ }
+  validates :last_name, presence: true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
+  validates :first_name, presence: true, format:{with: /\A(?:\p{Hiragana}|\p{Katakana}|[ー－]|[一-龠々])+\z/ }
   validates :last_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/ }
   validates :first_name_kana, presence: true, format:{with: /\A[ァ-ヶー－]+\z/ }
   validates :birth_date, presence: true
