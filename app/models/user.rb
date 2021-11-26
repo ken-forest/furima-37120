13c200ab6ab63f9772fc11ce63589dd3095510f2
ken-forest <0619green@gmail.com>
2021-11-26 11:24:22 +0900

    itamモデルとusrモデルにアソシエーションを記述
    ・itemモデルに以下を記述
    belongs_to :user
    has_one :purchase_record
    ・userモデルに以下を記述
    has_many :items
    has_many :purchase_records

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ApplicationRecord
+  
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
          
+  has_many :items
+  has_many :purchase_records
+
   validates :nickname, presence: true
   validates :last_name, presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
   validates :first_name, presence: true, format:{with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ }
