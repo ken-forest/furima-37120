c5b70a5dbc064ff4cbb6ff31e43d1fc838f46ce9
ken-forest <0619green@gmail.com>
2021-11-27 22:08:56 +0900

    カンマの抜け落ち、いらないカンマの削除、idの3が二つある問題を解消

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,12 +5,12 @@ class Item < ApplicationRecord
     belongs_to :fee_status
     belongs_to :prefecture
     belongs_to :scheduled_delivery
-  end
+  
 
 
 
   belongs_to :user
-  has_one :purchase_record
+  # has_one :purchase_record
   has_one_attached :image
 
   validates :image, presence: true
@@ -21,5 +21,5 @@ class Item < ApplicationRecord
   validates :fee_status_id, numericality: { other_than: 1 }, presence: true
   validates :prefecture_id, numericality: { other_than: 1 }, presence: true
   validates :scheduled_delivery_id, numericality: { other_than: 1 }, presence: true
-  validates :price, numericality: true, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ },
+  validates :price, numericality: { only_integer: true }, inclusion: { in: 300..9_999_999 }, format: { with: /\A[0-9]+\z/ }
 end
