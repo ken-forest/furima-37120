dac687874b56bfb1e8b1abd7d12379bb14ecb0f4
ken-forest <0619green@gmail.com>
2021-11-27 17:14:42 +0900

    ActiveHashを使うためのモデル・アソシエーション・バリデーションを設定
    ・item_categoryモデル
    ・item_sale_statusモデル
    ・fee_statusモデル
    ・prefectureモデル
    ・scheduled_deliveryモデル
    を追加
    itemモデルとそれぞれに関係する
    アソシエーションとバリデーションを追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,25 @@
 class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+    belongs_to :item_category
+    belongs_to :item_sale_status
+    belongs_to :fee_status
+    belongs_to :prefecture
+    belongs_to :scheduled_delivery
+  end
+
+
+
   belongs_to :user
   has_one :purchase_record
   has_one_attached :image
+
+  validates :image, presence: true
+  validates :item_name, presence: true
+  validates :detail, presence: true
+  validates :item_category_id, numericality: { other_than: 1 }, presence: true
+  validates :item_sale_status_id, numericality: { other_than: 1 }, presence: true
+  validates :fee_status_id, numericality: { other_than: 1 }, presence: true
+  validates :prefecture_id, numericality: { other_than: 1 }, presence: true
+  validates :scheduled_delivery_id, numericality: { other_than: 1 }, presence: true
+  
 end
