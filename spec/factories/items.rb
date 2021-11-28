32331579f63c23499924c21b5109644ba72b84c9
ken-forest <0619green@gmail.com>
2021-11-28 17:22:54 +0900

    異常系テスト、'imageが空では出品できない'テストコードを追加

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,6 +1,5 @@
 FactoryBot.define do
   factory :item do
-    # image                    {Faker::Lorem.sentence}
     item_name                {'明るい内装'}
     detail                   {'落ち着く'}
     item_category_id         {5}
@@ -11,8 +10,8 @@ FactoryBot.define do
     price                    {100000}
     user
 
-    after(:build) do |message|
-      message.image.attach(io: File.open('public/images/hero.jpg'), filename: 'hero.jpg')
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/hero.jpg'), filename: 'hero.jpg')
     end
 
 
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -10,11 +10,27 @@ RSpec.describe Item, type: :model do
 
   describe '商品の出品' do
 
+
       context '商品の出品ができる場合' do
 
         it 'image、item_name、detail、item_category_id、item_sale_status_id、fee_status_id、prefecture_id、scheduled_delivery_id、priceが存在すれば登録できる' do
           expect(@item).to be_valid
         end
+
+      end
+
+
+      context '商品の出品ができない場合' do
+
+        it 'imageが空では出品できない' do
+          @item.image = nil
+          @item.valid?
+          expect(@item.errors.full_messages).to include("Image can't be blank")
+        end
+
+        
+
+
       end
 
   end
