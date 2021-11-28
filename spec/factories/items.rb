95e36a26c56ee886356519682585d9ead2fd37d8
ken-forest <0619green@gmail.com>
2021-11-28 16:07:26 +0900

    商品の出品ができるテストコードを試作
    ・itemのFactoryBotのファイルにテスト用のダミーデータを作成
    ・spec/models/item_spec.rbに商品の出品が成功するテストコードを記述

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,20 @@
 FactoryBot.define do
   factory :item do
-    
+    # image                    {Faker::Lorem.sentence}
+    item_name                {'明るい内装'}
+    detail                   {'落ち着く'}
+    item_category_id         {5}
+    item_sale_status_id      {2}
+    fee_status_id            {3}
+    prefecture_id            {2}
+    scheduled_delivery_id    {3}
+    price                    {100000}
+    user
+
+    after(:build) do |message|
+      message.image.attach(io: File.open('public/images/hero.jpg'), filename: 'hero.jpg')
+    end
+
+
   end
 end
--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+
+
+  before do
+    @item = FactoryBot.build(:item)
+  end
+
+
+  describe '商品の出品' do
+
+      context '商品の出品ができる場合' do
+
+        it 'image、item_name、detail、item_category_id、item_sale_status_id、fee_status_id、prefecture_id、scheduled_delivery_id、priceが存在すれば登録できる' do
+          expect(@item).to be_valid
+        end
+      end
+
+  end
+
+
+
+
+
+
 end
