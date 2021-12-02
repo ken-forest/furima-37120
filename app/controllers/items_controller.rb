71374238d6eedf70207fa0b85ec5ec05e16c8a36
ken-forest <0619green@gmail.com>
2021-12-02 12:10:34 +0900

    before_actionのmove_to_top_pageの発動箇所にdestroyアクションを追加
    これにより、destroyアクションが発動する前に、、
    unless current_user == @item.user
          redirect_to root_path
     end
    、、の条件がかけられる事になる。

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
-  before_action :move_to_top_page, only: [:edit, :update]
+  before_action :move_to_top_page, only: [:edit, :update, :destroy]
   before_action :define_item, only: [:show, :edit, :update, :destroy]
 
 
