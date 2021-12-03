118b94d52410a3be6dd1dcd957d1fb457cf426c5
ken-forest <0619green@gmail.com>
2021-12-03 11:50:51 +0900

    order_shipping_addressモデルにデータを保存する記述を追加、その他
    バリデーションの「validates :order_id」を削除

--- app/models/order_shipping_address.rb
+++ app/models/order_shipping_address.rb
@@ -9,10 +9,17 @@ class OrderShippingAddress
     validates :city
     validates :address
     validates :phone_number, numericality: {only_integer: true, message: 'is invalid'}
-    validates :order_id
+    
+  end
+  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
+
+
+  def save
+    # 購入情報を保存し、変数orderに代入する
+    order = Order.create(user_id: user_id, item_id: item_id)
+    # 配送先の住所を保存する
+    shipping_address.create(postal_code: postal_code, prefecture_id: prefecture_id, city: city, address: address, building: building, phone_number: phone_number, order_id: order.id)
 
   end
-
-  validates :prefecture_id, numericality: { other_than: 1 , message: "can't be blank"}
 
 end
