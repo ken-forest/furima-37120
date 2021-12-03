0a581f44b0a53d455689e57a0428fc7041f744d8
ken-forest <0619green@gmail.com>
2021-12-03 15:50:57 +0900

    ordersコントローラのcreateアクションの中身を記述

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -8,7 +8,16 @@ class OrdersController < ApplicationController
   end
 
   def create
-    
+    @order_shipping_address = OrderShippingAddress.new(order_params)
+    @item = Item.find(params[:item_id])
+
+    if @order_shipping_address.valid?
+      @order_shipping_address.save
+      redirect_to root_path
+    else
+      render :index
+    end
+
   end
 
 
@@ -17,7 +26,7 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number)
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number, :user_id, :item_id).merge(user_id: current_user.id)
   end
 
 
