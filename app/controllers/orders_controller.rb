fd5dbb731de9ad00a1e81776886d6a014a0bef3e
ken-forest <0619green@gmail.com>
2021-12-03 12:39:56 +0900

    ordersコントローラのindexアクションに@order_shipping_addressを定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,2 +1,24 @@
 class OrdersController < ApplicationController
+
+
+  def index
+    @order_shipping_address = OrderShippingAddress.new
+
+  end
+
+  def create
+    
+  end
+
+
+
+
+  private
+
+  def order_params
+    params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number)
+  end
+
+
+
 end
