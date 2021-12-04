12d47ee5224a1952da8f1fbf36a88fd82e2c7dcd
ken-forest <0619green@gmail.com>
2021-12-04 16:55:41 +0900

    公開鍵を設定し、フォームの情報を取得して、それをトークン化

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -3,7 +3,23 @@ const pay = () => {
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
-    console.log("フォーム送信時にイベント発火");
+
+    const formResult = document.getElementById("charge-form");
+    const formData = new FormData(formResult);
+
+    const card = {
+      number: formData.get("order_shipping_address[card_number]"),
+      cvc: formData.get("order_shipping_address[cvc]"),
+      exp_month: formData.get("order_shipping_address[exp_month]"),
+      exp_year: `20${formData.get("order_shipping_address[exp_year]")}`,
+    };
+
+    Payjp.createToken(card, (status, response) => {
+      if (status == 200) {
+        const token = response.id;
+        console.log(token);
+      }
+    });
   });
 };
 
