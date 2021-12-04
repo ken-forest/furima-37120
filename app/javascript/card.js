0337840f4ae4f9471e7a484060436c131b7e7fb8
ken-forest <0619green@gmail.com>
2021-12-04 21:04:35 +0900

    クライアントサイドで環境変数を読み込めるように、JavaScriptで環境変数を呼び込めるようにした

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,5 +1,5 @@
 const pay = () => {
-  Payjp.setPublicKey("pk_test_db07b946bc336fbe89364b26");
+  Payjp.setPublicKey(process.env.PAYJP_PUBLIC_KEY);
   const submit = document.getElementById("button");
   submit.addEventListener("click", (e) => {
     e.preventDefault();
