e6bcdb5a3e3f357322d68166524cd9d85f403f1f
ken-forest <0619green@gmail.com>
2021-12-04 19:19:47 +0900

    決済処理をpay_itemメソッドとして定義して、praivate以下のランへ移動させた

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -13,12 +13,7 @@ class OrdersController < ApplicationController
 
     if @order_shipping_address.valid?
 
-      Payjp.api_key = "sk_test_f8f2221228f8c0c6d73ae9ac"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
-      Payjp::Charge.create(
-        amount: @item.price,  # 商品の値段
-        card: order_params[:token],    # カードトークン
-        currency: 'jpy'                 # 通貨の種類（日本円）
-      )
+      pay_item
 
       @order_shipping_address.save
       redirect_to root_path
@@ -35,6 +30,16 @@ class OrdersController < ApplicationController
 
   def order_params
     params.require(:order_shipping_address).permit(:postal_code, :prefecture_id, :city, :address, :building, :phone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+
+  def pay_item
+    Payjp.api_key = "sk_test_f8f2221228f8c0c6d73ae9ac"  # 自身のPAY.JPテスト秘密鍵を記述しましょう
+      Payjp::Charge.create(
+        amount: @item.price,  # 商品の値段
+        card: order_params[:token],    # カードトークン
+        currency: 'jpy'                 # 通貨の種類（日本円）
+      )
   end
 
 
